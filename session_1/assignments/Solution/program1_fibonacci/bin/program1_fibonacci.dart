0484757992305dca6bd5de5e7caa3fca1f6c3db4
JoelJojoP <joeljojop@gmail.com>
2022-01-10 16:56:34 +0530

    Solution for Program No. 1

---
+++ session_1/assignments/Solution/program1_fibonacci/bin/program1_fibonacci.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  int n = int.parse(stdin.readLineSync()!);
+  int t1 = 0;
+  int t2 = 1;
+  print("$t1\n$t2");
+  int t3;
+  for (var i = 2; i < n; i++) {
+    t3 = t1 + t2;
+    print("$t3");
+    t1 = t2;
+    t2 = t3;
+  }
+}
