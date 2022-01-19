8da40ce3d043908a8615ec6c6692a1595f996c36
suryansh <sgoyal2402@gmail.com>
2022-01-19 20:12:02 +0530

    trivia app with few animation

--- session_2/assingment/trivia/lib/main.dart
+++ session_2/assingment/trivia/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +14,176 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Number Trivia',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Number Trivia'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+//Stateless widget MyHomePage
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pinkAccent[100],
+      appBar: AppBar(
+        leading: const Icon(Icons.auto_awesome_outlined),
+        title: Text(
+          title,
+          style: GoogleFonts.comfortaa(),
+        ),
+      ),
+      body: const InputWidget(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//Widget for taking number input and passing to an api call
 
-  void _incrementCounter() {
+class InputWidget extends StatefulWidget {
+  const InputWidget({Key? key}) : super(key: key);
+
+  @override
+  _InputWidgetState createState() => _InputWidgetState();
+}
+
+class _InputWidgetState extends State<InputWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _ancontroller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..forward();
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _ancontroller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override
+  void dispose() {
+    _ancontroller.dispose();
+    super.dispose();
+  }
+
+  final TextEditingController _controller = TextEditingController();
+  final String _apiUrl = 'http://numbersapi.com/';
+  String _numberTrivia = '';
+  bool isLoading = false;
+
+  _handleSubmit(String value) {
+    if (value.isEmpty) {
+      return;
+    }
+    final Uri url = Uri.parse('$_apiUrl$value');
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isLoading = true;
     });
+    http.get(url).then((value) => setState(() {
+          if (value.statusCode == 200) {
+            _numberTrivia = value.body;
+          } else {
+            _numberTrivia = 'Something went wrong';
+          }
+          isLoading = false;
+          _controller.clear();
+          _ancontroller.forward(from: 0);
+        }));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return Builder(builder: (context) {
+      return SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              TextField(
+                  controller: _controller,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.megrim(
+                    textStyle: Theme.of(context).textTheme.headline4,
+                  ),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter a number',
+                      hintStyle: TextStyle(color: Colors.white70),
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 0.0, style: BorderStyle.none))),
+                  onSubmitted: _handleSubmit),
+              getCardWidget(),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
+    });
+  }
+
+  getCardWidget() {
+    if (isLoading) {
+      //return spinkit FadingCube
+      return Container(
+        margin: const EdgeInsets.only(top: 20.0),
+        padding: const EdgeInsets.all(16.0),
+        child: const SpinKitFadingCube(
+          color: Colors.white,
+          size: 30.0,
+        ),
+      );
+    } else if (_numberTrivia.isNotEmpty) {
+      return SizeTransition(
+        sizeFactor: _animation,
+        axis: Axis.vertical,
+        axisAlignment: -1.0,
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          color: Colors.pinkAccent[100],
+          elevation: 0.5,
+          margin: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              gradient: const LinearGradient(
+                colors: [
+                  Colors.red,
+                  Colors.pink,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      _numberTrivia,
+                      style: GoogleFonts.sacramento(
+                        fontSize: 28.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+    return Container();
   }
 }
