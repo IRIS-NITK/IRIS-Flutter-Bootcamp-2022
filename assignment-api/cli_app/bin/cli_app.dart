88220bd32ca0be459e66a0a1fbabb4c14e928e39
lohith <lohith1024@gmail.com>
2022-01-22 07:36:36 +0530

    working android app

--- assignment-api/cli_app/bin/cli_app.dart
+++ assignment-api/cli_app/bin/cli_app.dart
@@ -1,21 +1,27 @@
-import 'package:cli_app/cli_app.dart' as cli_app;
+
 import 'package:http/http.dart' as http;
 
 Future<void> main(List<String> arguments) async {
   String num='12';
-  print('Hello world: ${cli_app.calculate()}!');
+  print("input number is :$num");
   String resp= await getresp(num);
   print(resp);
 }
 
-Future<String> getresp(String i) async {
+Future<String> getresp(String i) async
+ {
   print("fetching responce from url");
-  var mod="http://numbersapi.com/"+i;
+try{
+var mod="http://numbersapi.com/"+i;
 var url = Uri.parse(mod);
 var response = await http.get(url);
 print('Response status: ${response.statusCode}');
 // print('Response body: ${response.body}');
 String body=response.body;
- 
-  return body;
+
+  return body;}
+  catch(e)
+  {
+    return "lost connection 🙄";
+  }
 }
---
+++ assignment-api/webr/lib/main.dart
@@ -0,0 +1,216 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+ 
+  Widget buildcard() => Card(
+        shadowColor: Colors.grey,
+        elevation: 12,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child:Column(
+         children: 
+          [
+            Stack(
+            alignment: Alignment.center,
+            children: [
+                
+              Ink.image(
+                // ignore: prefer_const_constructors
+                image:  NetworkImage(
+                 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1443&q=80'
+                  ),
+                 height:200,
+                 fit: BoxFit.cover,
+              ),
+              
+              const Positioned(
+                bottom: 11,
+                right: 11,
+                left: 10,
+                child: Text(
+                  "hi nice to see you",
+                  style:TextStyle(
+                    color: Colors.white, 
+                  ) ,
+                  ),
+              )
+            ],
+          ),
+        ],
+        
+        )
+      );
+ 
+
+ final TextEditingController _tc= TextEditingController();
+
+  void foo() {
+    if(_result!=0){
+      _result=0;
+    }
+  }
+  int _result=0;
+ String _str="";
+ Future<String> getresp(String i) async
+ {
+  // print("fetching responce from url");
+  var mod="http://numbersapi.com/"+i;
+var url = Uri.parse(mod);
+var response = await http.get(url);
+// print('Response status: ${response.statusCode}');
+// print('Response body: ${response.body}');
+String body=response.body;
+
+  return body;
+}
+
+ 
+ Future<void> fetch() async {
+   String str=_tc.text.toString();
+   String rep=await getresp(str);
+  setState(() {
+    _str=rep;
+  }); 
+   _result=1;
+   
+  }
+
+
+  Widget resultcard()=>Card(
+       shadowColor: Colors.red,
+        elevation: 20,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(22),
+        ),
+        child: Container(
+           decoration: BoxDecoration(
+            gradient: LinearGradient(
+            colors: [Colors.purpleAccent, Colors.purple.shade300],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          )),
+         padding:const  EdgeInsets.all(20),
+           child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+             children:[
+               Text(_result<0
+               ?' '
+               : _str),   
+               
+             ],
+           )
+        )
+  );
+
+
+  Widget beautifycard() => Card(
+        shadowColor: Colors.red,
+        elevation: 20,
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(22),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Colors.orangeAccent, Colors.orange.shade900],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          )),
+          padding: const EdgeInsets.all(40).copyWith(bottom: 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:  [
+             const Text(
+                "THIS IS A NUMBER TRIVIA ",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  ),
+              ),
+             TextField(
+              controller: _tc,
+               style: const TextStyle(
+                 color:Colors.white, 
+                 fontSize: 20
+                 ),
+               keyboardType: TextInputType.number,
+               inputFormatters: [
+                   FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),   
+               ],
+               maxLength:4 ,
+             ),
+              ButtonBar(
+               alignment: MainAxisAlignment.start,
+               children: [   
+                 ElevatedButton( 
+                   style: ElevatedButton.styleFrom(
+                     textStyle:const TextStyle(
+                       color: Colors.white,
+                     ),
+                    primary: Colors.blueGrey,
+                    onPrimary: Colors.blueGrey,
+                    onSurface:Colors.blueGrey,
+                   ),
+                  onPressed: fetch,
+                  child: const Text("fetch",
+                  style:TextStyle(
+                    color: Colors.white
+                  ),
+                  ),
+
+                  )
+               ],
+             )
+            ],
+
+          ),
+        ),
+      );
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: const Text("IRIS BOOT !!"),
+        ),
+        body: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+           
+             buildcard(),
+             beautifycard(),
+             resultcard()
+        
+          ],
+        )
+      ),
+    );
+  }
+
+
+ 
+}
+
