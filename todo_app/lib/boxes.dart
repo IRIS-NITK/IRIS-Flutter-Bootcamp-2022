8d66bf8ae0599828745d880dc3b4e8ef100fdddd
MohdDilshad-nitk <97335106+MohdDilshad-nitk@users.noreply.github.com>
2022-02-18 11:47:21 +0530

    Delete todo_app directory

--- todo_app/lib/boxes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hive/hive.dart';
-import 'package:todo_app/tasksmodel.dart';
-import 'tasksmodel.dart';
-
-class Boxes {
-  static Box<tasks> getTasks() => Hive.box<tasks>("todotasks");
-}
